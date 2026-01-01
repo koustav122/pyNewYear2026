6d57ec3262c2fb252695c4b1633264b50f2585c3
koustav122 <koustav1807.be22@chitkara.edu.in>
2026-01-01 22:28:23 +0530

    Initial commit

---
+++ HNY.py
@@ -0,0 +1,10 @@
+import time
+
+for i in range(5, 0, -1):
+    print(f"🎆🎉 Countdown: {i}")
+    time.sleep(1)
+
+print("\n🎉✨ HAPPY NEW YEAR 2026 ✨🎉")
+print("🌈 Cheers to new beginnings and endless possibilities 🌈🚀")
+
+
